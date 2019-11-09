35c062a4341c7d0bc2bfed3f73eeae2cce69e83c
alecfn <apfor2@student.monash.edu>
2019-11-09 20:13:42 +1100

    Tests and implementation of a Rotate command. The exact direction to set a robot to on a LEFT/RIGHT command will be determined by the CommandParser.

--- src/main/java/com/rea/toyrobot/commands/RotateCommand.java
+++ src/main/java/com/rea/toyrobot/commands/RotateCommand.java
@@ -1,5 +1,23 @@
 package com.rea.toyrobot.commands;
 
-public class RotateCommand implements CommandInterface {
+import com.rea.toyrobot.enums.Direction;
+import com.rea.toyrobot.model.Game;
 
+/**
+ * Rotates the robot on the board. The new direction will be determined by the CommandParser,
+ * based on whether the user input LEFT or RIGHT.
+ */
+public class RotateCommand implements GameOperation {
+
+  private Game game;
+  private Direction newDirection;
+
+  public RotateCommand(Game game, Direction newDirection) {
+    this.game = game;
+    this.newDirection = newDirection;
+  }
+
+  public void execute() {
+    game.getPlayerRobot().rotate(newDirection);
+  }
 }
--- src/test/java/com/rea/toyrobot/commands/RotateCommandTest.java
+++ src/test/java/com/rea/toyrobot/commands/RotateCommandTest.java
@@ -1,5 +1,34 @@
 package com.rea.toyrobot.commands;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import com.rea.toyrobot.enums.Direction;
+import com.rea.toyrobot.model.Game;
+import com.rea.toyrobot.model.Position;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
 public class RotateCommandTest {
+
+  private static Game testGame = new Game();
+  private RotateCommand testRotateCommand = new RotateCommand(testGame, Direction.NORTH);
+
+  @BeforeAll
+  static void setupGame() {
+    // Initialise a robot position before rotating the robot.
+    testGame.getPlayerRobot().place(new Position(0, 0), Direction.EAST);
+  }
+
+  @Test
+  void testRotateCommandRotatesRobot() {
+    assertEquals(Direction.EAST, testGame.getPlayerRobot().getCurrentDirection());
+    // Rotating from EAST to NORTH represents a LEFT command rotation.
+    testRotateCommand.execute();
+    assertEquals(Direction.NORTH, testGame.getPlayerRobot().getCurrentDirection());
+    // NORTH to EAST represents a RIGHT rotation.
+    testRotateCommand = new RotateCommand(testGame, Direction.EAST);
+    testRotateCommand.execute();
+    assertEquals(Direction.EAST, testGame.getPlayerRobot().getCurrentDirection());
+  }
 
 }
